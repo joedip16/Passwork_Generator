95793c5d403b2ae4a45f909a083310e28977bd00
joedip16 <joedippolito16@gmail.com>
2020-10-27 18:08:06 -0400

    created functions to generate characters

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var numberCharacters = prompt("How many characters would you like your password to contain?")
   passwordText.value = password;
 
 }
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+function getRandomSymbol(){
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(getRandomSymbol());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

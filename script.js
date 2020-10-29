bd07bf3d6e8cff7afa489f0dfda636daa3b1975a
joedip16 <joedippolito16@gmail.com>
2020-10-29 09:53:35 -0400

    created if else for number of characters

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 // Assignment Code
+var getSpecialCharacters = ["!", "@", "#", "^", "&", "*", "%", "+", "=",];
+var getRandomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var getRandomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var allCharacters = [getSpecialCharacters, getRandomLower, getRandomUpper, getRandomNumber];
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
   var numberCharacters = prompt("How many characters would you like your password to contain?")
-  passwordText.value = password;
+  if (numberCharacters > 8 && numberCharacters < 128) {
+    var getSpecialSymbol = confirm("Do you want speacial characters?")
+    var getRandomNumber = confirm("Do you want numerical characters?")
+    var getRandomUpper = confirm("Do you want uppercase characters")
+    var getRandomLower = confirm("Do you want lowercase characters")
+
+
+
+    var password = generatePassword(numberCharacters, getSpecialSymbol);
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  } else {
+    alert("Please use between 8 and 128 characters")
+  }
+  if { 
+
+  }
+
 
 }
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+
+function generatePassword(characters, hasSpecialSymbol) {
+  console.log("generate password!")
+  console.log(characters)
+  console.log(hasSpecialSymbol)
+  if (hasSpecialSymbol) {
+    console.log(getSpecialCharacters[8])
+  }
 }
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-function getRandomSymbol(){
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(getRandomSymbol());
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

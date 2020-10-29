7d2a39c2bfcaaeab43b2a723c02b71efe65e5a62
joedip16 <joedippolito16@gmail.com>
2020-10-29 17:21:16 -0400

    update for askBCS

--- script.js
+++ script.js
@@ -15,24 +15,30 @@ function writePassword() {
     var getRandomNumber = confirm("Do you want numerical characters?")
     var getRandomUpper = confirm("Do you want uppercase characters")
     var getRandomLower = confirm("Do you want lowercase characters")
-
+    console.log(numberCharacters)
 
 
     var password = generatePassword(numberCharacters, getSpecialSymbol);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-  } else {
+  } 
+  else {
     alert("Please use between 8 and 128 characters")
   }
-  if { 
+  
 
+  }
+  for (var i = 0; i < getRandomUpper.length; i++) {
+    var getRandomUpper = Math.floor(Math.random() * 10) +1;
+    console.log(getRandomUpper)
   }
 
 
-}
 
-function generatePassword(characters, hasSpecialSymbol) {
+
+
+function generatePassword(characters, hasSpecialSymbol, ) {
   console.log("generate password!")
   console.log(characters)
   console.log(hasSpecialSymbol)
@@ -45,3 +51,22 @@ function generatePassword(characters, hasSpecialSymbol) {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//store an array of lowercase characters in a variable
+//store and array of numeric characters in a variable
+//store an aray of upper cae chars in a variable
+//store an array of special chars ina  variable
+
+//function to prompt the user for theri password options
+  //variable to store the length of the password
+  //conditional statement to check if the password length is a true number
+  //chek to see if the password is at least 8 chars and less than 128
+  //need 4 confirms to check what chars user would like to use in the password
+  //conditional statement to check if user does not include anytypes of chars the generation will end
+  
+  //create an object to store user input
+
+
+  //function for getting a random element from an array
+
+  //

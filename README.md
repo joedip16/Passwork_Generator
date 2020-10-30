2b17d726168ed837ed323d7a4dff4917dccf2c4b
joedip16 <joedippolito16@gmail.com>
2020-10-30 17:19:12 -0400

    explained my process in the readme file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Passwork_Generator
+
+First, I created variables for each character type. Each character type went into an array.
+For the generate password function, I created variables to store the responses to the prompt and confirms.
+Then, I created the prompts and confirms for the user to chose the amount of characters and character type.
+Then, I created for loops for each character type that would go through the character types and select characters from the index. 
+Then, I created a for loop that would randomly select the characters based of the length of the desired password. 
+Lastly, I created a conditional that would alert the user if they didn't chose any character types that they have to chose at least one character type. 
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Assignment Code
-
-var specialChars = ["!", "@", "#", "^", "&", "*", "%", "+", "=",];
+var specialChars = ["!", "@", "#", "^", "&", "*", "%", "+", "="];
 var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -10,14 +8,12 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
-  console.log("generate password!")
   var confirmSpecial;
   var confirmLower;
   var confirmUpper;
   var confirmNumber;
 
   var numberCharacters = prompt("How many characters would you like your password to contain?")
-  console.log(numberCharacters)
   if (numberCharacters > 7 && numberCharacters < 129) {
     confirmSpecial = confirm("Do you want speacial characters?")
     confirmNumber = confirm("Do you want numerical characters?")
@@ -53,7 +49,7 @@ if(confirmUpper === true){
   }
 }
 
-if(confirmSpecial === false, confirmNumber === false, confirmUpper === false, confirmLower === false) {
+if(confirmSpecial === false && confirmNumber === false && confirmUpper === false && confirmLower === false) {
   alert("You must chose at least one type of character!")
   return
 }
@@ -75,60 +71,15 @@ console.log("--------------")
 console.log(characters)
 console.log(userPassword)
 
-
-
 return userPassword;
-
-
-
-  //first pass
-  //ask users for the length of the password
-  //check the minimun length of password
-  //if pass, ask user for their special character sets, save response to their varialbe 
-  //go through users confirm and include character sets of users choice into the pool of values character sets
-  //generate random number for the pool of characters inside loop
-  //include index using random number into the final passowrd inside loop
-  //return
-  
-
-  //second pass
-  //inlucde minimum of one character from the character sets
 
 }
 
-
-
-
-
-// Assignment Code
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//store an array of lowercase characters in a variable
-//store and array of numeric characters in a variable
-//store an aray of upper cae chars in a variable
-//store an array of special chars ina  variable
-
-//function to prompt the user for theri password options
-  //variable to store the length of the password
-  //conditional statement to check if the password length is a true number
-  //chek to see if the password is at least 8 chars and less than 128
-  //need 4 confirms to check what chars user would like to use in the password
-  //conditional statement to check if user does not include anytypes of chars the generation will end
-  
-  //create an object to store user input
-
-
-  //function for getting a random element from an array
